077a77ec857bf3ccc7bcd0632855df2f2a21d035
allelleo <dev.allelleo@internet.ru>
2024-03-10 00:52:21 +0300

    cos distance logic

--- clickhouse/connection.py
+++ clickhouse/connection.py
@@ -1,10 +1,39 @@
+import time
+
 import clickhouse_connect
+from scipy.spatial import distance
 
+start = time.time()
 client = clickhouse_connect.get_client(
-    host="127.0.0.1", username="test", password="test"
+    host="0e42-81-5-106-50.ngrok-free.app", username="test", password="test", port="80"
+)
+print("Ping:", client.ping())
+
+client.command(
+    """CREATE TABLE IF NOT EXISTS table4 (
+    Name String,
+    Url String,
+    Date String,
+    Number String,
+    Embedding Array(Float64)
+) ENGINE = MergeTree PRIMARY KEY tuple();"""
 )
 
-# client.command("SET allow_experimental_annoy_index = 1")
-client.command(
-    """CREATE TABLE table1 (Name String, embedding Array(Float32)) ENGINE = MergeTree ORDER BY Name;"""
-)
+
+# client.command(
+#     """INSERT INTO "table4"("Name","Url","Date","Number","Embedding") VALUES('alo','alo','alo','alo',[1,3.2]);"""
+# )
+search = [1, 2]
+print(f"Serach for vector: {search}")
+res = []
+fetch = client.query("SELECT * FROM table4")
+
+with fetch.rows_stream as stream:  # Тут есть коменты
+    for item in stream:
+        item = item[-1]
+        dist = distance.cosine(search, item)
+        res.append(dist)
+
+res.sort()
+print(res)
+print("Work: ", time.time() - start)
