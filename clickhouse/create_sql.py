f696237bb0c9191843c124bba33b4ff72e98431c
allelleo <dev.allelleo@internet.ru>
2024-03-13 00:02:22 +0300

    commit

---
+++ clickhouse/create_sql.py
@@ -0,0 +1,15 @@
+def generate_sql_row(
+    name: str,
+    url: str,
+    date: str,
+    number: str,
+    text: str,
+    vector: list[float],
+    table_name: str,
+):
+    numbers = ",".join([str(i) for i in vector])
+    return f"""INSERT INTO "{table_name}"("Name","Date","Text","Url","Number","Embedding") VALUES('{name}','{date}','{text}','{url}','{number}',({numbers}));"""
+
+
+sql = generate_sql_row("b", "a", "a", "a", "a", [i for i in range(1024)], "table7")
+print(sql)
